b5abe63d40df32cecd545bc5b07c03358d30a517
XL-YiBai <lei.xia@emqx.io>
2024-08-15 13:50:22 +0800

    feat: fifth update msg

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="第四次更新 Message" />
+      <HelloWorld msg="第五次更新 Message" />
     </div>
   </header>
 
